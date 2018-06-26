f27a6af0f1e821b9e67b4b276dc9195d83570c61
LiHao <1002732355@qq.com>
2018-06-26 19:38:42 +0800

    修改了package.appxmanifest 的基本设置

--- SerialPort.cs
+++ SerialPort.cs
@@ -22,5 +22,104 @@ namespace SerialPort
         ///打开串口开始连接
         ///</summary>
         ///<param name="portName">Name of COM port to open</param>
+        ///<param name="baudRate">baud rate of COM port 传输速率</param>
+        ///<param name="parity">type of data parity</param>
+        ///<param name="dataBits">Number of data bits</param>
+        ///<param name="stopbits">Number of stop</param>
+        public async Task<bool> Open(string portName, uint baudRate = 9600, SerialParity parity = SerialParity.None, ushort dataBits = 8, SerialStopBitCount stopBits = SerialStopBitCount.One)
+        {
+            //close open port 关闭当前正在打开的串口
+            //防止错误覆盖
+            Close();
+            
+            //get a list of devices from the given portname
+            string selector = SerialDevice.GetDeviceSelector(portName);
+
+
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+
+            return this.IsOpen;
+        }
+
+
+        ///////////////////////////////////////////////////////////////
+        ///<summary>
+        ///close the open port connection to the current device
+        ///关闭目标串口
+        ///</summary>
+        
+        public void Close()
+        {
+            //if serial device defined
+            if (this.SerialDevice != null)
+            {
+                this.SerialDevice.Dispose();
+                this.SerialDevice = null;
+            }
+
+            //if data reader is defined
+            if (this.dataReaderObject != null)
+            {
+                //dispose and clear stream
+                this.dataReaderObject.DetachStream();
+
+                //dispose and clear data reader
+                this.dataReaderObject.Dispose();
+                this.dataReaderObject = null;
+            }
+
+            //if data writer is defined
+            if (this.dataWriterObject != null)
+            {
+                //Datach writer stream
+                this.dataWriterObject.DetachStream();
+
+                //Dispose and clear data writer
+                this.dataWriterObject.Dispose();
+                this.dataWriterObject = null;
+            }
+
+            ///前面全是准备和清空工作
+
+            //Port now closed
+            this.IsOpen = false;
+        }
+
+        /////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Load serial data from the serial port  
+        /// 从串口中装载数据
+        /// </summary>
+        public void LoadSerialData(uint bufferSize)
+        {
+
+
+
+        }
+
+
+        /////////////////////////////////////////////////////////////
+        ///<summary>
+        ///Flag that indicates if COM port is open
+        ///</summary>
+        public bool IsOpen { get; set; }
+        /////////////////////////////////////////////////////////////
+        /// <summary>
+        /// the data reader used to read data from the COM port
+        /// </summary>
+        private SerialDevice SerialDevice;
+        /////////////////////////////////////////////////////////////
+        /// <summary>
+        /// the data writer used to send data from the COM port 
+        /// </summary>
+        private DataWriter dataWriterObject;
+
+        /////////////////////////////////////////////////////////////
+        /// <summary>
+        /// the data Reader used to send data to the COM port
+        /// </summary>
+        private DataReader dataReaderObject;
+
+
     }
 }
