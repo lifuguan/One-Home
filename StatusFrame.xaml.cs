03b40eeb36f57942af34aa75ce590007a0fd193b
LiHao <1002732355@qq.com>
2018-06-30 19:01:45 +0800

    POST成功 StatusCode: 200, ReasonPhrase: 'OK'

--- StatusFrame.xaml.cs
+++ StatusFrame.xaml.cs
@@ -13,9 +13,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Popups;
 using System.Net;
 using System.Text;
+using System.Net.Http;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -46,23 +46,23 @@ namespace SerialPort
         {
 
             this.InitializeComponent();
-            string portName = "COM";
-            for (int i = 0; i < 10; i++)
-            {
-                portName += i.ToString();
-                serialPort.Open(portName,this);   //取地址，不能通过new开辟新内存
-                portName = "COM";
-            }
+            //string portName = "COM";
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    portName += i.ToString();
+            //    serialPort.Open(portName, this);   //取地址，不能通过new开辟新内存
+            //    portName = "COM";
+            //}
 
-            //设置Timer来定时读取串口
-            _timer = new DispatcherTimer();
-            // Specifies the timer event interval.
-            // Runs here ONE second
-            _timer.Interval = TimeSpan.FromSeconds(1);
-            // indicate execute event
-            _timer.Tick += ProcSerialStream;
-            // Start a timer event
-            _timer.Start();
+            ////设置Timer来定时读取串口
+            //_timer = new DispatcherTimer();
+            //// Specifies the timer event interval.
+            //// Runs here ONE second
+            //_timer.Interval = TimeSpan.FromSeconds(1);
+            //// indicate execute event
+            //_timer.Tick += ProcSerialStream;
+            //// Start a timer event
+            //_timer.Start();
         }
 
         /// <summary>
@@ -78,9 +78,8 @@ namespace SerialPort
         string LightStatus_ = "";
         //红外检测
         string BodyDetect_ = "";
-        #region Process the serial data stream
 
-        
+        #region Process the serial data stream
 
         /// <summary>
         /// 处理数据流
@@ -88,14 +87,14 @@ namespace SerialPort
         private void ProcSerialStream(object sender, object e)
         {
             string ResString = ReadDataStream();
-            System.Diagnostics.Debug.WriteLine(ResString);
-            if (ResString.StartsWith("A")&&ResString.EndsWith(";"))
+            System.Diagnostics.Debug.WriteLine(ResString);   //输入到debug日志的信息
+            if (ResString.StartsWith("A") && ResString.EndsWith(";"))
             {
                 //测试用语句
                 //MessageDialog message_dialog = new MessageDialog(ResString, "退出");
                 //message_dialog.ShowAsync();   //不加await修饰符, No异步编程
                 string[] SplitData = ResString.Split(' ');
-                
+
                 if (SplitData.Length == 6)
                 {
                     int count = 0;
@@ -104,18 +103,22 @@ namespace SerialPort
                         if (count == 1)
                         {
                             Temperature_ = data;
+                            //Temperature.Text = Temperature_;
                         }
                         else if (count == 2)
                         {
                             Humidity_ = data;
+                            //Humidity.Text = Humidity_;
                         }
                         else if (count == 3)
                         {
                             FireStatus_ = data;
+                            //FireStatus.Text = FireStatus_;
                         }
                         else if (count == 4)
                         {
                             LightStatus_ = data;
+                            //LightStatus.Text = LightStatus_;
                         }
                         else if (count == 5)
                         {
@@ -133,11 +136,11 @@ namespace SerialPort
                 {
                     return;
                 }
-                
+
             }
         }
 
-
+        
 
         #endregion Process the serial data stream
 
@@ -197,14 +200,13 @@ namespace SerialPort
 
         #endregion Read the data stream
 
-
         #region Event Contral
         /// <summary>
         /// 控制显示COM UI的函数
         /// </summary>
         private void Hall_Click(object sender, RoutedEventArgs e)
         {
-
+            PostDataAsync("11", "22", "33", "44", "55");
         }
 
         private void RoomAlpha_Click(object sender, RoutedEventArgs e)
@@ -213,5 +215,39 @@ namespace SerialPort
         }
 
         #endregion Event Contral
+
+        #region Post Data
+
+        private static readonly HttpClient client = new HttpClient();
+        public async void PostDataAsync(string pTemperture, string pHumidity, string pFireStatus, string pLightStatus, string pBodyStatus)
+        {
+            var values = new Dictionary<string, string>
+                {
+                    {"count", "1" },
+                    {"temperture_0", pTemperture },
+                    {"Humidity_0", pHumidity },
+                    {"FireStatus_0", pFireStatus },
+                    {"LightStatus_0" ,pLightStatus},
+                    {"BodyDetect_0", pBodyStatus }
+                };
+            var content = new FormUrlEncodedContent(values);
+            try
+            {
+                var response = await client.PostAsync("http://115.159.36.210/api/onehome/upload", content);
+                System.Diagnostics.Debug.WriteLine(response);
+                var responseString = response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(responseString);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+         
+            
+        }
+        #endregion Post Data
     }
 }
